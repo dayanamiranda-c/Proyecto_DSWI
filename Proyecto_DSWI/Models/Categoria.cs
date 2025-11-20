787b78742ba0bfd80e73c24e805666cbb1daf060
jelias <elviscocho1998op@gmail.com>
2025-11-19 19:08:43 -0500

    Actualizacion.Entity

---
+++ Proyecto_DSWI/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_DSWI.Models
+{
+    public class Categoria
+    {
+        public short CategoriaId { get; set; }   // smallint
+        public string Nombre { get; set; }       // varchar(80)
+        public string Descripcion { get; set; } // varchar(255) NULL
+        public bool Activo { get; set; }         // bit
+        public DateTime UltimaActualizacion { get; set; } // datetime
+    }
+}
--- Proyecto_DSWI/Models/Producto.cs
+++ Proyecto_DSWI/Models/Producto.cs
@@ -3,10 +3,18 @@
 
     public class Producto
     {
-      public int id { get; set; }
-      public string name { get; set; }
+        public int ProductoId { get; set; }
+        public string Sku { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int CategoriaId { get; set; }
+        public string CodigoBarras { get; set; }
+        public decimal PrecioLista { get; set; }
+        public bool Activo { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime UltimaActualizacion { get; set; }
 
-      
+
 
     }
 }
